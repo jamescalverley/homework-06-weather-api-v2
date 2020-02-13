b5da0ca8ad7bcf6ea9f08fb1339ba2f1c48e0030
James Calverley <james.calverley3@gmail.com>
2020-02-13 17:25:29 -0500

    added search list

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-// remaing items
+// -------remaining items-------
+// add 5 day forecast
+// add search list functionality (add eventlistener to search button)
+// add search button functionality to list items 
+// localStorage / JSON / parse
+// add icons to current and forecast weather
+// add eventlistener on enter key
+// finishing all styling
+// make responsive
+// add limit to number of buttons in search list
 
 
+// search
+let searchCities = ["london", "seattle"];
+let city = "";
+
+document.getElementById('searchBtn').addEventListener('click', searchBar);
+
+function searchBar(){
+    city = document.getElementById('searchInput').value;
+    console.log(` search input: ${city}`)
+    searchCities.unshift(city);
+    console.log(`added to array: ${searchCities}`)
+    
+    // add search result to list
+    let node = document.createElement('BUTTON');
+    node.classList.add("list-group-item");
+    node.classList.add("list-group-item-action");
+    let textNode = document.createTextNode(city);node.appendChild(textNode);
+    document.getElementById('list-group').appendChild(node);
+    
+    // run current weather function
+    getWeatherData(city);
+};
 
 
-
-// API stuff
-let apiKey = "4e033b3f0bf4413196c595a89671e437";
-let cityInput = "Seattle"
-let cityName = cityInput.toLowerCase();
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-
-console.log(queryURL);
-
-//get api info
-function getWeatherData( cityName ){
+function getWeatherData( city ){
+    let apiKey = "4e033b3f0bf4413196c595a89671e437";
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -35,31 +59,9 @@ function getWeatherData( cityName ){
       }); 
 }
 
-getWeatherData();
-
-// Search 
-
-let searchCities = ["london", "seattle"];
-
-document.getElementById('searchBtn').addEventListener('click', searchBar);
-
-function searchBar(){
-    let search = document.getElementById('searchInput').value;
-    console.log(` search input: ${search}`)
-    searchCities.unshift(search);
-    console.log(`added to array: ${searchCities}`)
-    document.getElementById('searchResult').innerText = search;
-};
-
-// render search button list
-
-function renderSearchList(){
-    for(city in searchCities){
-        document.getElementById('list-group').innerText = city;
-    }
 
 
-};
+
 
 // UV Index
 
