10e858649aa37f05ade1ec76d09a9461f4dd6de7
James Calverley <james.calverley3@gmail.com>
2020-02-13 18:05:09 -0500

    added api call for 5 day forecast

--- script.js
+++ script.js
@@ -8,34 +8,39 @@
 // finishing all styling
 // make responsive
 // add limit to number of buttons in search list
+// **COMPLETE  clear search input form
+// add function to not add search item to list if input value is null
 
 
 // search
+let apiKey = "4e033b3f0bf4413196c595a89671e437";
 let searchCities = ["london", "seattle"];
 let city = "";
 
 document.getElementById('searchBtn').addEventListener('click', searchBar);
 
+// needs something to put city value into the api call
+document.getElementById('list-group').addEventListener('click', getWeatherData);
+
 function searchBar(){
     city = document.getElementById('searchInput').value;
+    document.getElementById('searchInput').value = "";
     console.log(` search input: ${city}`)
     searchCities.unshift(city);
     console.log(`added to array: ${searchCities}`)
-    
     // add search result to list
     let node = document.createElement('BUTTON');
     node.classList.add("list-group-item");
     node.classList.add("list-group-item-action");
     let textNode = document.createTextNode(city);node.appendChild(textNode);
     document.getElementById('list-group').appendChild(node);
-    
     // run current weather function
     getWeatherData(city);
+    getForecastData(city);
 };
 
-
+// current weather
 function getWeatherData( city ){
-    let apiKey = "4e033b3f0bf4413196c595a89671e437";
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     console.log(queryURL);
     $.ajax({
@@ -58,6 +63,36 @@ function getWeatherData( city ){
           document.getElementById('windSpeed').innerText = "Wind Speed: " + windSpeed + " km/h";
       }); 
 }
+
+// 5 day forecast
+function getForecastData(city){
+    
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; 
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }). then(function(response){
+        console.log(response);
+
+        let foreTemp1 = Math.floor((response.list[3].main.temp) - 273.15);
+        let foreHumidity1 = response.list[3].main.humidity;
+        let foreTemp2 = Math.floor((response.list[11].main.temp) - 273.15);
+        let foreHumidity2 = response.list[11].main.humidity;
+        let foreTemp3 = Math.floor((response.list[19].main.temp) - 273.15);
+        let foreHumidity3 = response.list[19].main.humidity;
+        let foreTemp4 = Math.floor((response.list[27].main.temp) - 273.15);
+        let foreHumidity4 = response.list[27].main.humidity;
+        let foreTemp5 = Math.floor((response.list[35].main.temp) - 273.15);
+        let foreHumidity5 = response.list[35].main.humidity;
+
+    });
+
+
+
+}
+
+getForecastData();
 
 
 
